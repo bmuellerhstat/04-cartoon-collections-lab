8ab07cc6087108775f91475a34a7f766b36ed978
meitingi2005 <meitingi2005@hstat.org>
2016-11-10 20:19:12 +0000

    HW

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(att)# code a parameter here
+  att.map.each_with_index do  |att,i|
+    puts "#{i+1}. #{att}"
+end
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+
+def summon_captain_planet(att)
+  att.collect do |name|
+    name.capitalize + "!"
+end
+  
+end
+ 
+def long_planeteer_calls(att)
+  att.any? do |word|
+     word.length > 4
+end
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
-end
-
-def find_the_cheese# code a parameter here
-  # the array below is here to help
+def find_the_cheese(att)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+cheese_types.find do |cheese|
+  att.include?(cheese)
+   
+end
 end
