ec8f0a5bc6940f4d813181ddcbd9b68ac77d2923
ziqianp0918 <ziqianp0918@hstat.org>
2016-11-10 19:23:06 +0000

    hw?

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+  dwarves.map.each_with_index do |x,y|
+    puts "#{y+1}. #{x}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planeteer_calls)# code a parameter here
   # Your code here
+  planeteer_calls.map do |x|
+   "#{x.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(arr)# code a parameter here
   # Your code here
+  arr.any? do |word|
+  word.length > 4
+end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(arr)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |x|
+    x == "cheddar"
+  end
 end
