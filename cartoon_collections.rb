798215d569ab2022137dd550615ca70ce8f8ccec
nievesalvin <alvinn4392@hstat.org>
2017-11-05 18:49:06 +0000

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do |name,i|
+    puts "#{i+1}, #{dwarfs}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(call)
+ if (call.size > 4)
+   return true
+ else 
+   return false
+ end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      return cheese
+    else
+      return nil
+    end
+  end
 end
