6758077029fc68771e61fef3aa78b8c85d11027c
biannak6288 <biannak6288@hstat.org>
2017-11-05 17:26:27 +0000

    fix errors in lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(names)# code a parameter here
+  names.each_with_index do |name, i|# Your code here
+  puts "#{i+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+def summon_captain_planet(arr)# code a parameter here
+  arr.map do |ele|# Your code here
+  "#{ele.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(calls)# code a parameter here
+  calls.length > 4? true : false# Your code here
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(arr)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if arr.include?(cheese)
+      return cheese
+    else
+      return nil
+    end
+  end
 end
