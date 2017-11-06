94c268adf123c2f6697b107c35f4f2fe5e3c2b24
abdelk7344 <abdelmonemmoamk7344@hstat.org>
2017-11-06 03:31:57 +0000

    There is always more than one way to accomplish a goal

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(array)# code a parameter here
   # Your code here
+  array.each_with_index do |dwarf,index|
+    puts "#{index+ 1}. #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(array_two)# code a parameter here
   # Your code here
+  array_two.collect do |element|
+    element.capitalize+"!"
+  end 
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(array_three)# code a parameter here
   # Your code here
+  [array_three].any? { |word| word.length >4 }
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(array_four)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  new_array=[]
+  array_four.each do |item|
+  if cheese_types.include?(item)
+    new_array.push(item)
+  end 
+  end 
+    new_array[0]
 end
