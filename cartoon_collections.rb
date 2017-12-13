73431a9ec1aad53b02d93bd19ff4aea5b1e3089f
Kellyx1636 <kellyx1636@hstat.org>
2017-12-13 09:01:34 +0000

    Long_planeteer_calls method unsuccessfully attempted.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
 dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"] 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]  
+short_words = ["puff", "go", "two"] 
+assorted_words = ["two", "go", "industrious", "bop"]
 
 def roll_call_dwarves(dwarf_names) # code a parameter here
   # Your code here 
-  # Lines 7-9: Takes the names in the array, print them out in a list form, while taking their index and starting it off with 1 instead of 0 by adding 1 to index
+  # Lines 9-11: Takes the names in the array, print them out in a list form, while taking their index and starting it off with 1 instead of 0 by adding 1 to index
   dwarf_names.each_with_index { |name, index| 
     puts "#{index+1}. #{name}"
   }
@@ -16,8 +18,24 @@ def summon_captain_planet(planeteer_calls) # code a parameter here
   }
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(short_words, assorted_words) # code a parameter here --> just 1 parameter?
+  # Your code here 
+  #Should there be two separate loops? 
+  short_words.each { |word| 
+    if short_words.length > 4 
+      true 
+    else 
+      false
+    end
+  }
+  
+   assorted_words.each { |word| 
+    if short_words.length > 4 
+      true 
+    else 
+      false
+    end
+  }
 end
 
 def find_the_cheese# code a parameter here
