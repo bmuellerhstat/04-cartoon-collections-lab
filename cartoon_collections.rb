0233b110db0d12d7b35d17aeb118b33c1ea16acd
Kellyx1636 <kellyx1636@hstat.org>
2017-12-13 08:47:16 +0000

    Summon_captain_planet method partially worked(?)

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,19 @@
 dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"] 
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]  
 
 def roll_call_dwarves(dwarf_names) # code a parameter here
   # Your code here 
-  # Lines 6-8: Takes the names in the array, print them out in a list form, while taking their index and starting it off with 1 instead of 0 by adding 1 to index
+  # Lines 7-9: Takes the names in the array, print them out in a list form, while taking their index and starting it off with 1 instead of 0 by adding 1 to index
   dwarf_names.each_with_index { |name, index| 
     puts "#{index+1}. #{name}"
   }
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planeteer_calls) # code a parameter here
   # Your code here
+  planeteer_calls.collect { |call| 
+    puts "#{call.capitalize}!"
+  }
 end
 
 def long_planeteer_calls# code a parameter here
