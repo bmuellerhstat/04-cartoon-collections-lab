cc3e50e4519df72fb7a9645f899ce72e0304cd1f
Kellyx1636 <kellyx1636@hstat.org>
2017-12-13 08:17:38 +0000

    Roll_call_dwarves method done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"] 
+
+def roll_call_dwarves(dwarf_names) # code a parameter here
+  # Your code here 
+  # Lines 6-8: Takes the names in the array, print them out in a list form, while taking their index and starting it off with 1 instead of 0 by adding 1 to index
+  dwarf_names.each_with_index { |name, index| 
+    puts "#{index+1}. #{name}"
+  }
 end
 
 def summon_captain_planet# code a parameter here
