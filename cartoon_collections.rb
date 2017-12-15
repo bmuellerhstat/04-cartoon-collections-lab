9949d4222492e30d07fae4ce87a938baa3488191
dakaraia <dakaraia6799@hstat.org>
2017-12-15 17:01:11 +0000

    finished cartoon

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,59 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(array)
+  hash = {}
+  (array).each_with_index { |item, index|
+  hash[item] = index
+  puts "#{index+1}. #{item}"
+  }
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+# dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+# roll_call_dwarves(dwarfs)
+
+
+def summon_captain_planet(array)
+  array.map! {|name| "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+# summon_captain_planet(planeteer_calls)
+
+
+def long_planeteer_calls(array)
+  (array).any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
+# short_words = ["puff", "go", "two"]
+# assorted_words = ["two", "go", "industrious", "bop"]
+
+# long_planeteer_calls(short_words)
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # array.include?(cheese_types)
+  # if (cheese_types & array).find{|common| print " #{common}" } == true
+  # else
+  #   print nil
+  # end
+  
+  # print (cheese_types & array).find{|common| print " #{common}" }
+  # return array.include?(cheese_types)
+
+  
+  array.find do |common|
+   cheese_types.include?(common)
+ 	end 
+  
+  
 end
+
+# snacks = ["crackers", "gouda", "thyme"]
+# soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+# ingredients = ["garlic", "rosemary", "bread"]
+
+
+# find_the_cheese(ingredients)
+
+
+
