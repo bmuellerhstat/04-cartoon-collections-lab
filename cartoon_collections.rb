a253a3af3c6dfad6511ed54d433950f1a778ab55
Kellyx1636 <kellyx1636@hstat.org>
2017-12-20 10:43:33 +0000

    Find_the_cheese method done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]  
 cheddar_cheese = ["banana", "cheddar", "sock"]
 
+#METHOD 1
 def roll_call_dwarves(dwarf_names) # code a parameter here
   # Your code here 
   # Lines 8-10: Takes the names in the array, print them out in a list form, while taking their index and starting it off with 1 instead of 0 by adding 1 to index
@@ -10,13 +11,15 @@ def roll_call_dwarves(dwarf_names) # code a parameter here
   }
 end
 
+#METHOD 2
 def summon_captain_planet(planeteer_calls) # code a parameter here
   # Your code here
   planeteer_calls.collect { |call| 
-    puts "#{call.capitalize}!"
+    puts call.capitalize + "!"
   }
 end
 
+#METHOD 3
 def long_planeteer_calls(planeteer_calls) # code a parameter here 
   # Your code here 
   planeteer_calls.each { |call| 
@@ -28,14 +31,18 @@ def long_planeteer_calls(planeteer_calls) # code a parameter here
   }
 end 
 
-def find_the_cheese(cheddar_cheese)# code a parameter here
+#METHOD 4
+def find_the_cheese(cheese_target)# code a parameter here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"] 
-  cheddar_cheese.each { |snack|
-    if snack.include?("cheddar") 
-      return "#{snack}"
-    else
-      return nil
-    end
-  }
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  new_cheese_array=[]
+  
+  cheese_target.each { |snack|
+  if cheese_types.include?(snack)
+    new_cheese_array.push(snack)
+  end 
+  } 
+    new_cheese_array[0]
 end
+
+
