4cb7db97db65b9f911de717db0e6b9914eb34dc2
JENNIFER LIU <jenniferl4209@hstat.org>
2018-11-01 22:33:47 +0000

    cartoon collection methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index do |dwarf,i|
+    puts "#{i+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect do |planeteer|
+    planeteer.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.length > 4
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  
+  end
 end
