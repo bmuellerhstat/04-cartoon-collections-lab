2524b015f0d73d7e16461ddf50944d5ca6b8aadc
Johnson Wu <johnsonw6558@hstat.org>
2018-11-01 23:10:49 +0000

    pass rspec

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+  numbered_dwarves = []
+  dwarves.each.with_index(1) do |i, dwarf|
+    numbered_dwarves << "#{dwarf}. #{i}"
+  end
+  puts numbered_dwarves
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planet_calls)# code a parameter here
   # Your code here
+  planet_calls.collect do |call|
+    call << "!"
+    call.capitalize
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(word_length)# code a parameter here
   # Your code here
+  word_length.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(food)# code a parameter here
+  food.find do |cheese| 
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
+
+# dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Happy", "Sleepy"]
+# roll_call_dwarves(dwarves)
+# planet_calls = ["earth", "wind", "fire", "water", "heart"]
+# summon_captain_planet(planet_calls)
+# assorted_words = ["two", "go", "industrious", "bop"]
+# long_planeteer_calls(assorted_words)
+# snacks = ["crackers", "gouda", "cheddar"]
+# find_the_cheese(snacks)
