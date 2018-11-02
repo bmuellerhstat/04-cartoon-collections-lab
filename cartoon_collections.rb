6c9e3aa6a503dd590bf9d39bb1374407bf7ca6ba
Ahmad Rabah <ahmadr9279@hstat.org>
2018-11-02 02:47:50 +0000

    Solve errors

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarve, i|
+    puts "#{i+1}. #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+def summon_captain_planet(captains)
+  captains.collect do |captain|
+    captain.capitalize+"!"
+  end
+  
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.length > 4
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
+
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+cheeses.find do |cheese|
+  cheese_types.include? (cheese)
+end
+
 end
