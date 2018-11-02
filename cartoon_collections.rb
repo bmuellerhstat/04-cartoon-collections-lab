21629f306970eb768e3ef8f74784d26f986323cc
Donia Abdelhalim <doniaa3710@hstat.org>
2018-11-02 03:54:28 +0000

    fix bugs

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarf)# code a parameter here
   # Your code here
+  dwarf.each do |i|
+    puts (dwarf.index(i)+1).to_s + '. '+ i.to_s
+  end 
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(dwarf)# code a parameter here
   # Your code here
+  dwarf.each do |i|
+    i.capitalize! << '!'
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(dwarf)# code a parameter here
   # Your code here
+  if dwarf.count > 4
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(dwarf)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  dwarf.each do |i|
+    cheese_types.each do |k|
+      if i == k
+        return k
+      end
+    end
+  end
+  return nil
 end
