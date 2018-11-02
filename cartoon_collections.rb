d89fc3fb4debf4ef6d06fb19dd0b0e65411fcd61
Xiurong Yu <xiurongy3506@hstat.org>
2018-11-02 01:38:07 +0000

    practice iteration, methods, and if else statement

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+    dwarves.each.with_index(1) do |dwarf, i|
+     puts "#{i}. #{dwarf}"
+   end 
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet (veggies)# code a parameter here
   # Your code here
+  veggies.collect do |veggie|
+      veggie.capitalize + "!"
+  end
+  
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(short_planeteer_calls)# code a parameter here
   # Your code here
+  if short_planeteer_calls.length >=4 
+    true
+  else 
+    false
+  end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(cheddar_cheese)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_cheese.find do |cheese|
+    cheese_types.include? (cheese)
+  end
 end
