4d1cabcc0bc5f272d15498bfaf13ed4f8d72700e
Wei Zheng <weiz9762@hstat.org>
2018-11-02 01:27:14 +0000

    utilize find,include,collect method and conditional to achieve requirements

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planeteers)# code a parameter here
   # Your code here
+  planeteers.collect do |planeteer|
+    planeteer.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(calls)# code a parameter here
+  # Your code here 
+  calls.length >= 4 ? true : false
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(arr)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |word|
+    cheese_types.any? do |chess|
+      chess == word 
+    end
+  end
 end
