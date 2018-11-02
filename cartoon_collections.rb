8c2291fb576751ae23141c1741ee2eaf2a927d81
Toby lau <yatchol6214@hstat.org>
2018-11-02 11:55:58 +0000

    chesse for cheese

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(dwarves)# code a parameter here
+  dwarves.each.with_index(1) do |dwarfs, i|
+    puts "#{i}. #{dwarfs}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planeteer_calls)# code a parameter here
   # Your code here
+  planeteer_calls.each do |cap|
+    cap.capitalize!
+    cap << "!"
+  end
+  planeteer_calls
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(length)# code a parameter here
   # Your code here
+  length.each do |word|
+    if word.length>4
+      return true
+    else 
+     return false
+    end
+  end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(cheese)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  
+  end  
 end
