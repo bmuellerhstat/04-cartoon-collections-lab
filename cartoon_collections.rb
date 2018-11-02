7c0e6d6b2b3602a912fb96e604a3e3e577a692cb
Arieta Haskaj <arietah8545@hstat.org>
2018-11-02 05:15:53 +0000

    pass rspec tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |person, i|
+    puts "#{i}. #{person}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.each do |word|
+    word.capitalize!
+    word << "!"
+  end
+  planeteer_calls
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |word|
+    if word.length > 4
+      return true
+    else
+      return false
+    end
+  end
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
+def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  items.find do |cheese|
+    cheese_types.include?(cheese)
+    # items.any? {|x| cheese_types.include? (x)}
+  end
 end
