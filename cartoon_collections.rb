2bde176965be54daebab7a954a541d021d44d312
Sherzod <sherzodm8441@hstat.org>
2018-11-02 03:49:46 +0000

    cartoon_collections.rb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+  # dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarves.each.with_index(1) { |dwarf, i|
+  puts "#{i} #{dwarf}"
+  }
+  
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planeteer_calls)# code a parameter here
   # Your code here
+   
+   planeteer_calls.collect { |element|
+    element.capitalize + "!"
+  }
+  
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(any_array)# code a parameter here
   # Your code here
+  any_array.each { |word|
+  if word.length > 4
+    return true
+  else
+    return false
+  end
+  }
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(any_array)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  if any_array.include?(cheese_types[0] || word == cheese_types[1] || word == cheese_types[2] ) 
+    any_array.each {|word|
+    if word == cheese_types[0] || word == cheese_types[1] || word == cheese_types[2]
+      return word
+    end
+    }
+ else 
+   return nil
+  end
+  
 end
