3d59199e80810f50c322c62cdaa6fb4a8b3efca3
Yu Hang Chen <yuhangc9321@hstat.org>
2018-11-02 02:30:20 +0000

    define methods to pass tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+  dwarves.each.with_index(1) do |dwarf, i|
+    puts "#{i}. #{dwarf}"
+    
+  end
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planeteer_calls)# code a parameter here
   # Your code here
+  planeteer_calls.map do |planeteer_call|
+    "#{planeteer_call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(planeteer_calls)# code a parameter here
   # Your code here
+  planeteer_calls.any? do |planeteer_call|
+    planeteer_call.length > 4
+  end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(things)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  things.find do |item|
+    cheese_types.include?(item)
+  end
 end
