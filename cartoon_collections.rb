374261895089e4ffe695fc2d201355a49e56e951
Xavier Jiang <xavierj2216@hstat.org>
2018-11-02 00:45:21 +0000

    finish cartoon homework

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(dwarves)# code a parameter here
   # Your code here
+   dwarves_names = []
+   
+    dwarves.each.with_index(1) do |dwarf, position|
+    dwarves_names << "#{position}. #{dwarf}"
+  end
+  
+  puts dwarves_names
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(planet_calls)# code a parameter here
   # Your code here
+  planet_calls.map do |call|
+    call += "!"
+    call.capitalize!
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(call_length)# code a parameter here
   # Your code here
+    call_length.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
+
+
+def find_the_cheese(cheeses)# code a parameter here
+# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # cheese.include? cheese_type
+  
+  cheeses.find do |cheese| 
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
