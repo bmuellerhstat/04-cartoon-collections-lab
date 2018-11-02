5c3e8e60d50a401877df0d059a547d7142c07f05
Raquel Joseph <raquelj6437@hstat.org>
2018-11-02 01:47:11 +0000

    pass rspec

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves(list)# code a parameter here
   # Your code here
+  list.each do |i|
+    puts (list.index(i)+1).to_s + '. '+ i.to_s
+  end 
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(list)# code a parameter here
   # Your code here
+  list.each do |i|
+    i.capitalize! << '!'
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(list)# code a parameter here
   # Your code here
+  if list.count > 4
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(list)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.each do |i|
+    cheese_types.each do |k|
+      if i == k
+        return k
+      end
+    end
+  end
+  return nil
 end
