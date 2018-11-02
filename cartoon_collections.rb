982674fdc9907a2bef44b5b37ae3c32d4146611e
Travis Matos <travism7618@hstat.org>
2018-11-02 12:37:40 +0000

    finishes lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code a parameter here
+def roll_call_dwarves (names)# code a parameter here
   # Your code here
+  names.each.with_index(1) do |name,number|
+    puts "#{number}. #{name}"
+  end
 end
 
-def summon_captain_planet# code a parameter here
+def summon_captain_planet(array)# code a parameter here
   # Your code here
+  array.map do |word|
+     word.capitalize! << "!"
+  end
 end
 
-def long_planeteer_calls# code a parameter here
+def long_planeteer_calls(calls)# code a parameter here
   # Your code here
+  calls.each do |call|
+     if call.length>4
+       return true
+     else 
+       return false
+     end
+  end
 end
 
-def find_the_cheese# code a parameter here
+def find_the_cheese(array)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      return cheese
+    else 
+     return nil
+    end 
+  end
 end
