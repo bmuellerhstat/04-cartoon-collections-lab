2ffd1eb02a4550e4ab9d54f4639d3d02317a0051
Kasper Gacek <kasperg3971@hstat.org>
2018-11-12 19:36:51 +0000

    Redone cartoon_collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,23 +6,23 @@ def roll_call_dwarves(dwarves)# code a parameter here
 end
 
 def summon_captain_planet(planeteer_calls)# code a parameter here
-  # Your code here\ 
-  planeteer_calls.collect do |element|
-    "#{element.capitalize}!"
-  end
-end
-
-def long_planeteer_calls(long)# code a parameter here
   # Your code here
-    long.any? do |word|
-      word.length > 4
+  planeteer_calls.map do |element|
+    "#{element.capitalize}!"
     end
 end
+def long_planeteer_calls(long_words)# code a parameter here
+  # Your code here
+  long_words.any? do |word|
+    word.length > 4
+  end
+end
 
-def find_the_cheese(cheeses)# code a parameter here
+def find_the_cheese(types)# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    cheeses.find do |cheese| 
+  types.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
+  
 end
